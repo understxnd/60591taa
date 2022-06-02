f4080d8423bb92e118646ba19163e6a621cf98d7
understxnd <alex.targonskiy@yandex.ru>
2022-06-03 01:21:40 +0500

    проект HTML Академия

--- auth.php
+++ auth.php
@@ -1,20 +1,36 @@
 <?php
 session_start();
-if ($_POST['login']){
+if($_POST['login'])
+{
     $result = $conn->query("SELECT * FROM users WHERE login='".$_POST['login']."'");
-
-    if ($row = $result->fetch()){
-        if (md5($_POST['password']) == $row['password']){
+    if($row = $result->fetch())
+    {
+        if(($_POST['password']) == $row['password'])
+        {
             $_SESSION['username'] = $_POST['login'];
+            $_SESSION['id_auth_user'] = $row['id'];
+            $_SESSION['is_admin'] = $row['is_admin'];
         }
-        else{
+        else
+        {
             $message = 'Неверный пароль';
         }
     }
-    else{
+    else
+    {
         $message = 'Неверный логин';
     }
-}
-if ($_GET['logout']){
+
+    $title = $row['title'];
+    $img_url = $row['img_url'];
+    $price = $row['price'];
+    if($_POST['password'] == 'pass')
+    {
+        $_SESSION['username'] = $_POST['login'];
+    }
+};
+
+if($_GET['logout'])
+{
     session_unset();
 }
